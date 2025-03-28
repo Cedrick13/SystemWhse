e4def98b0ff3b58349ed50986fc6a67c7b21e0b7
Cedrick Embestro <cedrick13wew@gmail.com>
2025-03-28 15:40:21 +0800

    Delete Button

--- SystemWhse/Forms/FormDashboard.Designer.cs
+++ SystemWhse/Forms/FormDashboard.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -60,33 +57,6 @@
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(423, 358);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 22);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(553, 358);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 22);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(721, 375);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(62, 22);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -290,9 +260,6 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "FormDashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.FormDashboard_Load);
@@ -314,9 +281,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
