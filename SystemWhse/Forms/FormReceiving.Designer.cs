f93f63166bf44edd61b5e5bcec4d30bbfd877657
Cedrick Embestro <cedrick13wew@gmail.com>
2025-04-03 11:26:31 +0800

    Add Footer Receiving

--- SystemWhse/Forms/FormReceiving.Designer.cs
+++ SystemWhse/Forms/FormReceiving.Designer.cs
@@ -44,6 +44,8 @@
             this.btnprev = new System.Windows.Forms.Button();
             this.btnfirst = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,7 +85,7 @@
             "20",
             "50",
             "100"});
-            this.comboBox1.Location = new System.Drawing.Point(65, 523);
+            this.comboBox1.Location = new System.Drawing.Point(64, 492);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(45, 21);
             this.comboBox1.TabIndex = 67;
@@ -94,7 +96,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label3.Location = new System.Drawing.Point(9, 524);
+            this.label3.Location = new System.Drawing.Point(9, 494);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 16);
             this.label3.TabIndex = 66;
@@ -143,7 +145,7 @@
             this.Label9.AutoSize = true;
             this.Label9.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.Label9.Location = new System.Drawing.Point(314, 524);
+            this.Label9.Location = new System.Drawing.Point(314, 494);
             this.Label9.Name = "Label9";
             this.Label9.Size = new System.Drawing.Size(173, 16);
             this.Label9.TabIndex = 58;
@@ -154,7 +156,7 @@
             this.btnlast.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnlast.BackColor = System.Drawing.Color.Transparent;
             this.btnlast.ForeColor = System.Drawing.Color.Black;
-            this.btnlast.Location = new System.Drawing.Point(757, 517);
+            this.btnlast.Location = new System.Drawing.Point(757, 489);
             this.btnlast.Name = "btnlast";
             this.btnlast.Size = new System.Drawing.Size(31, 29);
             this.btnlast.TabIndex = 59;
@@ -166,7 +168,7 @@
             this.btnnext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnnext.BackColor = System.Drawing.Color.Transparent;
             this.btnnext.ForeColor = System.Drawing.Color.Black;
-            this.btnnext.Location = new System.Drawing.Point(722, 517);
+            this.btnnext.Location = new System.Drawing.Point(722, 489);
             this.btnnext.Name = "btnnext";
             this.btnnext.Size = new System.Drawing.Size(31, 29);
             this.btnnext.TabIndex = 60;
@@ -178,7 +180,7 @@
             this.btnprev.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnprev.BackColor = System.Drawing.Color.Transparent;
             this.btnprev.ForeColor = System.Drawing.Color.Black;
-            this.btnprev.Location = new System.Drawing.Point(687, 517);
+            this.btnprev.Location = new System.Drawing.Point(687, 489);
             this.btnprev.Name = "btnprev";
             this.btnprev.Size = new System.Drawing.Size(31, 29);
             this.btnprev.TabIndex = 61;
@@ -190,7 +192,7 @@
             this.btnfirst.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnfirst.BackColor = System.Drawing.Color.Transparent;
             this.btnfirst.ForeColor = System.Drawing.Color.Black;
-            this.btnfirst.Location = new System.Drawing.Point(652, 517);
+            this.btnfirst.Location = new System.Drawing.Point(652, 489);
             this.btnfirst.Name = "btnfirst";
             this.btnfirst.Size = new System.Drawing.Size(31, 29);
             this.btnfirst.TabIndex = 62;
@@ -205,8 +207,29 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 91);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 413);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 396);
             this.dataGridView1.TabIndex = 57;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(219)))), ((int)(((byte)(221)))));
+            this.label4.Location = new System.Drawing.Point(301, 539);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 13);
+            this.label4.TabIndex = 79;
+            this.label4.Text = "© 2025 timecargo.ph All rights reserved.";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(219)))), ((int)(((byte)(221)))));
+            this.panel1.Location = new System.Drawing.Point(0, 534);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 24);
+            this.panel1.TabIndex = 80;
             // 
             // FormReceiving
             // 
@@ -214,6 +237,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(250)))), ((int)(((byte)(252)))));
             this.ClientSize = new System.Drawing.Size(800, 558);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
@@ -251,5 +276,7 @@
         internal System.Windows.Forms.Button btnprev;
         internal System.Windows.Forms.Button btnfirst;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
