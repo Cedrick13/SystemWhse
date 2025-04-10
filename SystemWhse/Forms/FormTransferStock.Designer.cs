72eb2f66765d82f1e7b7455ecd9dc2a256a155c2
Cedrick Embestro <cedrick13wew@gmail.com>
2025-04-10 10:26:24 +0800

    BorderStyle Fixed Single

--- SystemWhse/Forms/FormTransferStock.Designer.cs
+++ SystemWhse/Forms/FormTransferStock.Designer.cs
@@ -91,12 +91,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(250)))), ((int)(((byte)(252)))));
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(800, 558);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.Label6);
             this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormTransferStock";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
