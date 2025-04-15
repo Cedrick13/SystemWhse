7f1da11d397c8c3ff33e926b36793a5fd5c12389
Cedrick Embestro <cedrick13wew@gmail.com>
2025-04-15 10:32:43 +0800

    Cancel Button on Add Opening

--- SystemWhse/Forms/FormAddOpening.cs
+++ SystemWhse/Forms/FormAddOpening.cs
@@ -25,5 +25,16 @@ namespace SystemWhse.Forms
             this.Text = string.Empty;
             this.ControlBox = false;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to cancel?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return; // Exit without doing anything
+            }
+
+            // Proceed with cancel action
+            this.Close(); // Close the form (if applicable)
+        }
     }
 }
