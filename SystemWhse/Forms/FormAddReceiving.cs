0d9e4562fa160fda5babdac7f8595182cb88aa12
Cedrick Embestro <cedrick13wew@gmail.com>
2025-04-16 11:12:35 +0800

    ComboBox Function on Customer & Disable TextBox & Show Value TextBox

--- SystemWhse/Forms/FormAddReceiving.cs
+++ SystemWhse/Forms/FormAddReceiving.cs
@@ -11,7 +11,7 @@ using MySql.Data.MySqlClient;
 
 namespace SystemWhse.Forms
 {
-    public partial class FormAddReceiving: Form
+    public partial class FormAddReceiving : Form
     {
         string connectionString = "server=192.168.1.230;user=Server;password=12345;database=tlcwms;";
         private MySqlCommand command;
@@ -19,6 +19,8 @@ namespace SystemWhse.Forms
         private DataTable table;
         private MySqlConnection connection;
         private string connStr;
+        Dictionary<string, string> customerMap = new Dictionary<string, string>();
+
         public FormAddReceiving()
         {
             InitializeComponent();
@@ -28,7 +30,7 @@ namespace SystemWhse.Forms
 
         private void FormAddReceiving_Load(object sender, EventArgs e)
         {
-
+            LoadComboBoxData();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -41,5 +43,47 @@ namespace SystemWhse.Forms
             // Proceed with cancel action
             this.Close(); // Close the form (if applicable)
         }
+
+        private void LoadComboBoxData()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT custcode, name FROM customer";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string code = reader.GetString("custcode");
+                        string name = reader.GetString("name");
+
+                        comboBox4.Items.Add(code); // still show code in comboBox
+                        customerMap[code] = name;  // map code to name
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+
+            comboBox4.SelectedIndexChanged += comboBox4_SelectedIndexChanged;
+            textBox3.Enabled = false;
+            textBox5.Enabled = false;
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCode = comboBox4.SelectedItem.ToString();
+
+            if (customerMap.ContainsKey(selectedCode))
+            {
+                textBox3.Text = customerMap[selectedCode]; // Display the customer name
+            }
+        }
     }
 }
+
